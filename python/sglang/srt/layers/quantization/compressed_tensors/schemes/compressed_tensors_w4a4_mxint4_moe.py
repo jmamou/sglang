842f3a8c2fff4d6971e0876d1e2865f861831d9b
jmamou <jonathan.mamou@intel.com>
2026-04-15 03:50:41 -0700

    is_flashinfer_available fix

--- python/sglang/srt/layers/quantization/compressed_tensors/schemes/compressed_tensors_w4a4_mxint4_moe.py
+++ python/sglang/srt/layers/quantization/compressed_tensors/schemes/compressed_tensors_w4a4_mxint4_moe.py
@@ -33,15 +33,18 @@ if TYPE_CHECKING:
     )
 
 if is_flashinfer_available():
-    from flashinfer.fp4_quantization import block_scale_interleave
-    from flashinfer.fused_moe import (
-        convert_to_block_layout,
-        trtllm_mxint4_block_scale_moe,
-    )
-    from flashinfer.fused_moe.core import (
-        _maybe_get_cached_w3_w1_permute_indices,
-        get_w2_permute_indices_with_cache,
-    )
+    try:
+        from flashinfer.fp4_quantization import block_scale_interleave
+        from flashinfer.fused_moe import (
+            convert_to_block_layout,
+            trtllm_mxint4_block_scale_moe,
+        )
+        from flashinfer.fused_moe.core import (
+            _maybe_get_cached_w3_w1_permute_indices,
+            get_w2_permute_indices_with_cache,
+        )
+    except ImportError:
+        pass
 
 
 class CompressedTensorsMxInt4MoE(CompressedTensorsMoEScheme):
